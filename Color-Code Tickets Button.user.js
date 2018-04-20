3c989c2242c68acd9d8bdd4e20bb28a69f0d2124
Taylor Griffin <taylor.griffin.365@gmail.com>
2018-04-20 11:34:46 -0700

    added * to path

--- Color-Code Tickets Button.user.js
+++ Color-Code Tickets Button.user.js
@@ -4,6 +4,6 @@
 // @version      1.0
 // @description  Allows one to toggle color-coding of tickets based on ticket type
 // @author       Taylor Griffin
-// @match        https://oregonstate.teamdynamix.com/TDNext/Home/Desktop/Default.aspx
+// @match        https://oregonstate.teamdynamix.com/TDNext/Home/Desktop/Default.aspx*
 // @grant        none
 // ==/UserScript==
