0db10325c364f129648aee3eb6e66e9cd8471384
milettal <32145791+milettal@users.noreply.github.com>
2018-05-03 10:58:11 -0700

    Extra

--- Color-Code Tickets Button.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Color-Code Tickets Toggle
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Allows one to toggle color-coding of tickets based on ticket type
-// @author       Taylor Griffin
-// @match        https://oregonstate.teamdynamix.com/TDNext/Home/Desktop/Default.aspx*
-// @grant        none
-// ==/UserScript==
