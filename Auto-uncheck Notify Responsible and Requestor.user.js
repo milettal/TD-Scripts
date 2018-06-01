5ba947ab5448f3bb27513c9551bdca495a8678fa
milettal <32145791+milettal@users.noreply.github.com>
2018-06-01 16:47:33 -0700

    Fixed Bug

--- Auto-uncheck Notify Responsible and Requestor.user.js
+++ Auto-uncheck Notify Responsible and Requestor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto-uncheck Notify Responsible and Requestor
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Auto-uncheck Notify Responsible
 // @author       Luke Miletta
 // @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/New*
@@ -11,7 +11,7 @@
 
 var checks = document.getElementsByClassName("checkbox");
 for(var i = 0; i < checks.length; i++){
-    if((checks[i].children)[0].innerText == "Notify Requestor" || (checks[i].children)[0].innerText == "Notify Responsible"){
+    if((checks[i].children)[0].innerText == "Notify Requestor" || (checks[i].children)[0].innerText == "Notify Responsible" || (checks[i].children)[0].innerText == "Notify on change"){
         (((checks[i].children)[0]).children)[0].click();
     }
 }
