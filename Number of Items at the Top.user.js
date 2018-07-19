20d0ffeaa39e7c232690cfc5f09736d27c036e0c
farnhamt <farnhamt@oregonstate.edu>
2018-07-19 13:16:15 -0700

    Update Number of Items at the Top.user.js

--- Number of Items at the Top.user.js
+++ Number of Items at the Top.user.js
@@ -3,30 +3,24 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.2
 // @description  Grabs the number of items in the queue and pastes it in the top
-// @author       Luke Miletta
+// @author       Luke Miletta / Tyler Farnham
 // @match        https://oregonstate.teamdynamix.com/TDNext/Home/Desktop/*
 // @grant        none
 // ==/UserScript==
 
 window.setTimeout(items, 1500);
-var iii;
 
 function items(){
-    //var boxes = document.querySelectorAll('[dragclass=DragDragBox]');
-    var boxes = document.getElementsByClassName("report-module");
-    /*boxes.forEach(function(element){
-        if((((element.childNodes)[0]).childNodes)[0].textContent == 'SD - open, unassigned (Incidents, Service Requests)'){
-            iii = element.id;
-        }
-    });*/
-    for (var i = 0; i < boxes.length; i++) {
-        if((((boxes[i].childNodes)[0]).childNodes)[0].textContent == 'SD - open, unassigned (Incidents, Service Requests)'){
-            iii = boxes[i].id;
+    var ticketTable = document.getElementsByClassName("ModuleContent")[1].childNodes[1].childNodes[3].childNodes;
+    var numTickets = 0;
+    for(var i = 0; i < ticketTable.length; i++){
+        if(ticketTable[i].nodeName == "TR") {
+            numTickets++;
         }
     }
-    var numitems = (((((document.getElementById(iii).childNodes)[1]).childNodes)[3]).childNodes)[3].textContent;
-    numitems = "<br>" + numitems + "in the Queue";
-    var htmlString = '<div style="Font-Size: 40px; text-align:center;">' + numitems + '</div>';
+
+    var numTicketsText = "<br>" + numTickets + " tickets in the Queue";
+    var htmlString = '<div style="Font-Size: 40px; text-align:center;">' + numTicketsText + '</div>';
     var divv = document.createElement('div');
     divv.innerHTML = htmlString;
     var topp = document.getElementById('divContent');
