4f3b6174cec11b9876f48a11e9aaa4648f73fa91
farnhamt <farnhamt@oregonstate.edu>
2018-08-15 16:07:34 -0700

    Update Auto-Sort By Status.user.js

--- Auto-Sort By Status.user.js
+++ Auto-Sort By Status.user.js
@@ -1,25 +1,30 @@
 // ==UserScript==
 // @name         Auto-Sort By Status
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Autosorts the SD - open tickets queue to by status
 // @author       Luke Miletta
 // @match        https://oregonstate.teamdynamix.com/TDNext/Home/Desktop*
-// @grant        GM_setValue
-// @grant        GM_getValue
 // ==/UserScript==
 
 var id;
-
+window.setTimeout(items, 100);
 function items(){
     var boxes = document.getElementsByClassName("report-module");
+    if(!boxes.length){return;}
     for (var i = 0; i < boxes.length; i++) {
         if((((boxes[i].childNodes)[0]).childNodes)[0].textContent == 'SD - open, unassigned (Incidents, Service Requests)'){
+
             id = boxes[i].id;
+            console.log(id);
         }
     }
     boxes = document.getElementById(id);
-
+    console.log(boxes);
+    if(!boxes){
+        window.setTimeout(items, 100);
+        return;
+    }
     if(boxes.getElementsByClassName("sort-link")[4]){
         boxes = boxes.getElementsByClassName("sort-link")[4].childNodes[0];
         eventFire(boxes, 'click');
