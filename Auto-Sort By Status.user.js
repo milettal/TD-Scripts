f5c2e9b3a0da52629ff93417a9dbdb688d93f414
farnhamt <farnhamt@oregonstate.edu>
2018-08-15 15:56:59 -0700

    Update Auto-Sort By Status.user.js

--- Auto-Sort By Status.user.js
+++ Auto-Sort By Status.user.js
@@ -8,16 +8,7 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
-var tdRunCount = GM_getValue("tdRunCount", 0);
-if(tdRunCount == 0){
-    GM_setValue("tdRunCount", 1);
 
-}
-
-else{
-    window.setTimeout(items, 1000);
-    GM_setValue("tdRunCount", 0);
-}
 var id;
 
 function items(){
@@ -27,21 +18,24 @@ function items(){
             id = boxes[i].id;
         }
     }
-   boxes = document.getElementById(id);
+    boxes = document.getElementById(id);
 
     if(boxes.getElementsByClassName("sort-link")[4]){
-   boxes = boxes.getElementsByClassName("sort-link")[4].childNodes[0];
-           eventFire(boxes, 'click');
+        boxes = boxes.getElementsByClassName("sort-link")[4].childNodes[0];
+        eventFire(boxes, 'click');
     }
-    else{window.setTimeout(items, 100);}
+    else{
+        console.log("Waiting");
+        window.setTimeout(items, 100);
+    }
 }
 
 function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
+    if (el.fireEvent) {
+        el.fireEvent('on' + etype);
+    } else {
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
+    }
 }
